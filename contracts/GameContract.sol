dd5b8bb72e841684a862381d809a4baffdf77afd
Viet Hoang <ngviethoang0212@gmail.com>
2021-12-30 22:08:15 +0700

    update contract

--- contracts/GameContract.sol
+++ contracts/GameContract.sol
@@ -1,214 +1,322 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-contract oan_tu_ti{
+contract OneTwoThree {
+    uint256 constant SCISSORS = 1;
+    uint256 constant ROCK = 2;
+    uint256 constant PAPER = 3;
 
+    enum RESULT_TYPE {
+        UNDECIDED,
+        WIN_1,
+        DRAW,
+        WIN_2
+    }
 
-    uint constant Scissors = 0;
-    uint constant Rock = 1;
-    uint constant Paper = 2;
-    
-    uint constant Win_1 = 1;
-    uint constant Draw = 0;
-    uint constant Win_2 = 2;
-
-    uint constant Empty = 0;
-    uint constant One_person = 1;
-    uint constant Full = 2;
-
-    uint choose_2 = 3;
-    uint check_2 = 0;
-
-
-    uint choose_1 = 3;
-    uint check_1 = 0;
-
+    enum ROOM_STATUS {
+        EMPTY,
+        ONE_PERSON,
+        FULL,
+        PLAYED
+    }
 
     struct Room {
+        uint256 id;
         address Address_1;
         address Address_2;
-        uint Status ;
-        bytes32 Guess_1;
-        bytes32 Guess_2;
-        uint Result;
-        uint Bet_amount;
-        uint check_1 ;
-        uint check_2 ;
-        }
-
-        
-     
-
-    Room[100] public arrRoom; 
-
-    function Room_status(uint number) public view returns (uint256) {
-        return arrRoom[number].Status ;
+        uint256 Bet_amount;
+        ROOM_STATUS status;
+        RESULT_TYPE result;
+        // Player's option in hashcode
+        bytes32 hashcode_1;
+        bytes32 hashcode_2;
+        // Player's option in number (rock, scissors, paper), set after sending secret key
+        // if choose = 0 => player haven't sent secret key
+        uint256 choose_1;
+        uint256 choose_2;
+        // Check if a player sent secret key or not
+        bool check_1;
+        bool check_2;
     }
 
-    function room_status() public view returns (uint[100] memory) {
-        uint[100] memory arrroom_status;
-        for(uint i; i < 100; i++) {
-            arrroom_status[i] = arrRoom[i].Status; 
-        }
+    mapping(uint256 => Room) public rooms;
 
-        return arrroom_status;
+    // Keep track of opened room number
+    uint256 public roomNumber;
+
+    // Check number of opening rooms (1 person)
+    uint256 public openingRoomNumber;
+
+    // Check if someone is already a owner of a opening room or not
+    // If they were, stop them from opening a new one
+    mapping(address => uint256) public ownerCount;
+
+    // Check number of rooms a player has joined
+    mapping(address => uint256) public playerRoomsCount;
+
+    event CreateRoom(Room _room);
+    event JoinRoom(Room _room);
+    event MakeResult(Room _room);
+    event Withdraw(Room _room, address _player);
+
+    constructor() {}
+
+    function balance() public view returns (uint256) {
+        return address(this).balance;
     }
 
-
-
-
-    function uint2str(uint256 _i) internal pure returns (string memory str){
-        if (_i == 0){
-            return "0";
+    function getOpeningRooms() public view returns (Room[] memory) {
+        Room[] memory results = new Room[](openingRoomNumber);
+        uint256 rid = 0;
+        for (uint256 i; i < roomNumber; i++) {
+            if (rooms[i].status == ROOM_STATUS.ONE_PERSON) {
+                results[rid] = rooms[i];
+                rid += 1;
+            }
         }
-        uint256 j = _i;
-        uint256 length;
-        while (j != 0){
-           length++;
-           j /= 10;
+        return results;
+    }
+
+    function getPlayerRooms(address _player)
+        public
+        view
+        returns (Room[] memory)
+    {
+        uint256 count = playerRoomsCount[_player];
+        Room[] memory results = new Room[](count);
+        uint256 rid = 0;
+        for (uint256 i; i < roomNumber; i++) {
+            if (
+                rooms[i].Address_1 == _player || rooms[i].Address_2 == _player
+            ) {
+                results[rid] = rooms[i];
+                rid += 1;
+            }
         }
-        bytes memory bstr = new bytes(length);
-        uint256 k = length;
-        j = _i;
-        while (j != 0){
-           bstr[--k] = bytes1(uint8(48 + j % 10));
-           j /= 10;
+        return results;
+    }
+
+    // Send bet amount to create a room
+    function createRoom() public payable {
+        require(
+            ownerCount[msg.sender] == 0,
+            "You are already a owner of other room."
+        );
+        require(msg.value > 0, "bet amount must greater than 0");
+
+        ownerCount[msg.sender] = ownerCount[msg.sender] + 1;
+        playerRoomsCount[msg.sender] = playerRoomsCount[msg.sender] + 1;
+
+        Room storage room = rooms[roomNumber];
+        room.id = roomNumber;
+        room.Address_1 = msg.sender;
+        room.Bet_amount = msg.value;
+        room.status = ROOM_STATUS.ONE_PERSON;
+
+        emit CreateRoom(room);
+
+        openingRoomNumber += 1;
+        roomNumber += 1;
+    }
+
+    // Player send option here to join a room
+    function sendHashcode(uint256 _id, bytes32 _hashcode) public payable {
+        Room storage room = rooms[_id];
+        // With owner: Only send option if room is full (after other player sent hashcode)
+        if (room.Address_1 == msg.sender) {
+            require(room.status == ROOM_STATUS.FULL, "Room is unavailable");
+            room.hashcode_1 = _hashcode;
         }
-        str = string(bstr);
-        return str;
+        // With guest player: Only send option if room has one person
+        else {
+            require(
+                room.status == ROOM_STATUS.ONE_PERSON,
+                "Room is unavailable"
+            );
+            require(
+                msg.value == room.Bet_amount,
+                "You must bet equal in the first 1"
+            );
+            room.status = ROOM_STATUS.FULL;
+            room.hashcode_2 = _hashcode;
+            room.Address_2 = msg.sender;
+
+            playerRoomsCount[msg.sender] = playerRoomsCount[msg.sender] + 1;
+
+            openingRoomNumber -= 1;
+
+            emit JoinRoom(room);
+        }
     }
 
     // Hàm rút tiền . Một trong 2 người chơi đều có quyền rút tiền khi 1 trong 2 chưa gửi key bí mật lên hợp đồng.
     // Nếu chủ phòng là người rút thì tiền cược 2 người chơi sẽ về lại tài khoản của họ và phòng trở về trạng thái available
     // Nếu người chơi 2 rút tiền thì họ sẽ nhận lại tiền và phòng trở về trạng thái 1.
-    function withdraw(uint _number)  public payable {
-        require(arrRoom[_number].Status == Full || arrRoom[_number].Status == One_person, "You can not withdraw");
-        require(msg.sender == arrRoom[_number].Address_1 || msg.sender == arrRoom[_number].Address_2,"You are not players in this room");
-        require(arrRoom[_number].check_1 == 0,"You can not withdraw");
-        require(arrRoom[_number].check_2 == 0,"You can not withdraw");
-        if (msg.sender == arrRoom[_number].Address_1) {
-            payable(msg.sender).transfer(arrRoom[_number].Bet_amount);
-            payable(arrRoom[_number].Address_2).transfer(arrRoom[_number].Bet_amount);
-            arrRoom[_number].Status = Empty;
-        }
+    function withdraw(uint256 _id) public payable {
+        Room storage room = rooms[_id];
+        require(
+            msg.sender == room.Address_1 || msg.sender == room.Address_2,
+            "You are not players in this room"
+        );
+        require(!room.check_1 && !room.check_2, "You can not withdraw");
 
-        if (msg.sender == arrRoom[_number].Address_2) {
-            arrRoom[_number].Status = One_person;
-            payable(msg.sender).transfer(arrRoom[_number].Bet_amount);
-        }
-    }
- 
-    
+        if (msg.sender == room.Address_1) {
+            payable(msg.sender).transfer(room.Bet_amount);
 
-    
-    function select_guess(uint _number, bytes32 hashcode) public payable  returns (string memory description)  {
-       if (arrRoom[_number].Status == Empty) {
-            arrRoom[_number].Address_1 = msg.sender ;
-            arrRoom[_number].Bet_amount = msg.value;
-            arrRoom[_number].Guess_1 = hashcode ;
-            arrRoom[_number].Status = One_person;
-       } else {
-                require(arrRoom[_number].Address_1 != msg.sender,"You already selected");
-                require(msg.value == arrRoom[_number].Bet_amount,"You must bet equal in the first 1  ");
-                require(arrRoom[_number].Status == One_person);
-                arrRoom[_number].Status = Full;
-                arrRoom[_number].Guess_2 = hashcode ;
-                arrRoom[_number].Address_2 = msg.sender ;
-         }
-    }
-
-    function input_secret(string memory sercet_code, uint _number) public payable returns (string memory description) {
-        require(arrRoom[_number].Status == Full,"please wait");
-        if (msg.sender == arrRoom[_number].Address_1) {
-            bytes memory c;
-            for (uint i; i < 3; i++){
-                c = bytes.concat(bytes(sercet_code),bytes(uint2str(i)));
-                if (sha256(c) == arrRoom[_number].Guess_1) {
-                    choose_1 = i;
-                    check_1 = 1;
-                    break;
-                }
+            if (room.status == ROOM_STATUS.FULL) {
+                // If this room has 2 players, send money back to both of them
+                payable(room.Address_2).transfer(room.Bet_amount);
+            } else if (room.status == ROOM_STATUS.ONE_PERSON) {
+                openingRoomNumber -= 1;
             }
-            require(choose_1 != 3,"the first sercet_code is wrong");
-            arrRoom[_number].check_1 = check_1;    
+            room.status = ROOM_STATUS.EMPTY;
+            ownerCount[msg.sender] = 0;
+        } else if (msg.sender == room.Address_2) {
+            room.status = ROOM_STATUS.ONE_PERSON;
+            openingRoomNumber += 1;
+
+            // Delete player 2 data in this room
+            room.Address_2 = address(0);
+            room.hashcode_2 = "";
+
+            payable(msg.sender).transfer(room.Bet_amount);
+
+            playerRoomsCount[msg.sender] = playerRoomsCount[msg.sender] - 1;
         }
-        if (msg.sender == arrRoom[_number].Address_2) {
-            bytes memory c;
-            for (uint i; i < 3; i++){
-                c = bytes.concat(bytes(sercet_code),bytes(uint2str(i)));
-                if (sha256(c) == arrRoom[_number].Guess_2) {
-                    choose_2 = i;
-                    check_2 = 1;
-                    break;
-                }
-            }
-            require(choose_2 != 3,"the second sercet_code is wrong");
-            arrRoom[_number].check_2 = check_2;    
-        } 
 
-        if (arrRoom[_number].check_1 !=0 && arrRoom[_number].check_2 !=0) {
-            make_result(choose_1,choose_2,_number);
-                if (arrRoom[_number].Result == Draw){
-                    payable (arrRoom[_number].Address_1).transfer(arrRoom[_number].Bet_amount);
-                    payable (arrRoom[_number].Address_2).transfer(arrRoom[_number].Bet_amount);
-                    description  = " Draw";
-                }
-
-                if (arrRoom[_number].Result == Win_1){
-                    description = "The first is win";
-                    payable (arrRoom[_number].Address_1).transfer(arrRoom[_number].Bet_amount*2);
-                   
-                }
-                
-                if (arrRoom[_number].Result == Win_2){
-                    description = " The secound is win";
-                    payable (arrRoom[_number].Address_2).transfer(arrRoom[_number].Bet_amount*2);
-                }
-                arrRoom[_number].Status = Empty;
-
-        }             
-        return description;
+        emit Withdraw(room, msg.sender);
     }
 
-    function make_result(
-        uint256 choose_1,
-        uint256 choose_2,
-        uint256 _number
-    ) private {
-        if (choose_1 == Scissors && choose_2 == Rock) {
-            arrRoom[_number].Result = Win_2;
+    // After both players sent hashcode, continue by sending secret code
+    function sendSecret(uint256 _id, string memory _secretCode) public payable {
+        Room storage room = rooms[_id];
+        require(room.status == ROOM_STATUS.FULL, "Room is unavailable");
+
+        // Find sender's option by secret key
+        if (msg.sender == room.Address_1) {
+            uint256 choose = getPlayerOption(_secretCode, room.hashcode_1);
+            require(choose > 0, "the first secret code is wrong");
+            room.check_1 = true;
+            room.choose_1 = choose;
+        } else if (msg.sender == room.Address_2) {
+            require(
+                room.hashcode_1 != "",
+                "Player 1 haven't sent option. Please wait."
+            );
+            uint256 choose = getPlayerOption(_secretCode, room.hashcode_2);
+            require(choose > 0, "the second secret code is wrong");
+            room.check_2 = true;
+            room.choose_2 = choose;
         }
 
-        if (choose_1 == Scissors && choose_2 == Paper) {
-            arrRoom[_number].Result = Win_1;
+        // If both players sent secret key, make result and send money to winner
+        if (room.check_1 && room.check_2) {
+            room.result = getResult(room.choose_1, room.choose_2);
+            room.status = ROOM_STATUS.PLAYED;
+            ownerCount[room.Address_1] = 0;
+
+            if (room.result == RESULT_TYPE.DRAW) {
+                payable(room.Address_1).transfer(room.Bet_amount);
+                payable(room.Address_2).transfer(room.Bet_amount);
+            } else if (room.result == RESULT_TYPE.WIN_1) {
+                payable(room.Address_1).transfer(room.Bet_amount * 2);
+            } else if (room.result == RESULT_TYPE.WIN_2) {
+                payable(room.Address_2).transfer(room.Bet_amount * 2);
+            }
+
+            emit MakeResult(room);
+        }
+    }
+
+    /**
+    Pure functions
+    **/
+    function getResult(uint256 _choose_1, uint256 _choose_2)
+        private
+        pure
+        returns (RESULT_TYPE result)
+    {
+        if (_choose_1 == SCISSORS && _choose_2 == ROCK) {
+            return RESULT_TYPE.WIN_2;
         }
 
-        if (choose_1 == Scissors && choose_2 == Scissors) {
-            arrRoom[_number].Result = Draw;
+        if (_choose_1 == SCISSORS && _choose_2 == PAPER) {
+            return RESULT_TYPE.WIN_1;
         }
 
-        if (choose_1 == Rock && choose_2 == Scissors) {
-            arrRoom[_number].Result = Win_1;
+        if (_choose_1 == SCISSORS && _choose_2 == SCISSORS) {
+            return RESULT_TYPE.DRAW;
         }
 
-        if (choose_1 == Rock && choose_2 ==Paper) {
-            arrRoom[_number].Result = Win_2;
+        if (_choose_1 == ROCK && _choose_2 == SCISSORS) {
+            return RESULT_TYPE.WIN_1;
         }
 
-        if (choose_1 == Rock && choose_2 == Rock) {
-            arrRoom[_number].Result = Draw;
+        if (_choose_1 == ROCK && _choose_2 == PAPER) {
+            return RESULT_TYPE.WIN_2;
         }
 
-        if (choose_1 == Paper && choose_2 == Scissors) {
-            arrRoom[_number].Result = Win_2;
+        if (_choose_1 == ROCK && _choose_2 == ROCK) {
+            return RESULT_TYPE.DRAW;
         }
 
-        if (choose_1 == Paper && choose_2 == Paper) {
-            arrRoom[_number].Result = Draw;
+        if (_choose_1 == PAPER && _choose_2 == SCISSORS) {
+            return RESULT_TYPE.WIN_2;
         }
 
-        if (choose_1 == Paper && choose_2 == Rock) {
-            arrRoom[_number].Result = Win_1;
+        if (_choose_1 == PAPER && _choose_2 == PAPER) {
+            return RESULT_TYPE.DRAW;
         }
+
+        if (_choose_1 == PAPER && _choose_2 == ROCK) {
+            return RESULT_TYPE.WIN_1;
+        }
+    }
+
+    function getPlayerOption(string memory _secretCode, bytes32 _hashcode)
+        private
+        pure
+        returns (uint256)
+    {
+        if (getHashcode(_secretCode, SCISSORS) == _hashcode) {
+            return SCISSORS;
+        }
+        if (getHashcode(_secretCode, ROCK) == _hashcode) {
+            return ROCK;
+        }
+        if (getHashcode(_secretCode, PAPER) == _hashcode) {
+            return PAPER;
+        }
+        return 0;
+    }
+
+    function getHashcode(string memory _secretCode, uint256 _option)
+        public
+        pure
+        returns (bytes32)
+    {
+        return
+            sha256(bytes.concat(bytes(_secretCode), bytes(uint2str(_option))));
+    }
+
+    function uint2str(uint256 _i) internal pure returns (string memory str) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        j = _i;
+        while (j != 0) {
+            bstr[--k] = bytes1(uint8(48 + (j % 10)));
+            j /= 10;
+        }
+        str = string(bstr);
+        return str;
     }
 }
